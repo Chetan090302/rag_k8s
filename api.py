02a1ce0d16738d594087b7ac179e7d3c1f450018
JETTY CHETAN <jettychetan524@gmail.com>
2026-09-20 11:26:12 +0530

    added apis

--- api.py
+++ api.py
@@ -14,6 +14,10 @@ class Output(BaseModel):
 
 app=FastAPI()
 
+@app.get("/root")
+def get_root():
+    return {"response":"Hello chetan"}
+
 @app.get("/")
 def get_root():
     return {"response":"Hello world"}

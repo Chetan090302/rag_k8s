285a7bcce9055a5d1e6045dcfb591225be1ba5b7
JETTY CHETAN <jettychetan524@gmail.com>
2026-09-20 11:18:05 +0530

    increased replica count

--- api.py
+++ api.py
@@ -3,9 +3,6 @@ from rag_pipeline import trigger_graph
 from pydantic import BaseModel
 from typing import Optional
 
-#force push
-#hello world
-
 class Input(BaseModel):
     question:str
     answer:str=""
